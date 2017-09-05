4427abf22e30034da713c18eba81d932adc55687
Adam Patridge <patridge@gmail.com>
2017-09-05 11:37:03 -0600

    Initial course exercises import.

---
+++ Exercise 1/Completed/Debugging/Person.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Debugging
+{
+	public class Person 
+    {
+		public string Name     { get; set; }
+		public int    Age      { get; set; }
+		public int    Siblings { get; set; }
+
+		public string HaveBirthday() 
+        {
+            return string.Format("{0} is {1} years old!", Name, ++Age);
+		}
+
+		public string DescribeFamily() 
+        {
+            string siblingText;
+
+            if (Siblings == 0)
+			{
+                siblingText = " has no siblings";
+            }
+			else
+			{
+                siblingText = " has " + Siblings + " siblings";
+            }
+
+            return Name + siblingText;
+		}
+
+        public static List<Person> GetPeople()
+		{
+            var people = new List<Person>
+			{
+                new Person { Name = "Rachel", Age = 20, Siblings = 0 },
+				new Person { Name = "Bobby",  Age = 16, Siblings = 2 },
+				new Person { Name = "John",   Age = 35, Siblings = 1 },
+				new Person { Name = "Mary",   Age = 40, Siblings = 4 },
+			};
+
+			return people;
+		}
+	}
+}
---
+++ Exercise 1/Start/Debugging/MainPage.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+using System.Collections.Generic;
+
+namespace Debugging
+{
+    public class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            List<string> messages = new List<string>();
+
+            messages.Add("Happy Birthday Everyone!");
+
+            var people = Person.GetPeople();
+
+            foreach (Person p in people)
+			{
+                messages.Add( p.HaveBirthday() );
+                messages.Add( p.DescribeFamily() );
+            }
+
+            Content = new ListView { ItemsSource = messages };
+        }
+    }
+}
